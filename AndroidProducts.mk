a5d94225c090275d0c4acbcf90868770b772b79a
ation_ciger <ation_ciger@sgxi.cn>
2023-02-01 20:20:07 +0800

    first commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MKAFILES := \
+	$(LOCAL_DIR)/rubens.mk
+
+COMMON_LUNCH_CHOICES := \
+	rubens-eng \
+	rubens-userdebug \
+	rubens-user
