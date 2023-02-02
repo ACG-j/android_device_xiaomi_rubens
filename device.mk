ddf20933f764577ce133efc324e4a18e106aa61b
ation_ciger <ation_ciger@sgxi.cn>
2023-02-02 14:07:12 +0800

    add lineage overlay

--- device.mk
+++ device.mk
@@ -237,7 +237,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
     FrameworkResOverlay \
