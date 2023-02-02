9b72b01e42448387dbd4c588af4fc3b6533e13c0
ation_ciger <ation_ciger@sgxi.cn>
2023-02-02 14:01:53 +0800

    fix product errors

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,10 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-PRODUCT_MKAFILES := \
-	$(LOCAL_DIR)/aosp_rubens.mk
+PRODUCT_MAKEFILES := \
+	$(LOCAL_DIR)/rubens.mk
 
 COMMON_LUNCH_CHOICES := \
-	aosp_rubens-eng \
-	aosp_rubens-userdebug \
-	aosp_rubens-user
+	rubens-eng \
+	rubens-userdebug \
+	rubens-user
